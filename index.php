e0cb5a0e1a34a26f76bd58e819bbe83d247cae81
FOG <dn135897@gmail.com>
2024-10-17 00:05:05 +0700

    Add logo, header, footer - 17/10/2024

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <!-- Trang chính của website. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D2P Ticket Cinema</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon.ico">
+</head>
+
+<body>
+
+    <?php
+
+    // Include header
+    include 'includes/header.html';
+
+    // Include body content
+    include 'includes/body.html';
+
+    // Include footer
+    include 'includes/footer.html';
+
+    ?>
+</body>
+
+</html>
