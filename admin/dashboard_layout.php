8f5e72ba452497504943565444f93fb775c13ecd
FOG <dn135897@gmail.com>
2024-10-17 15:14:20 +0700

    Thêm layout_dashboard - 17/10/2024
    Nhằm mục đích sử dụng lại các element

---
+++ admin/dashboard_layout.php
@@ -0,0 +1,16 @@
+<?php
+// Include header (chứa các liên kết CSS và meta)
+include __DIR__ . '\header.php';
+
+// Include menu (chứa navbar và sidebar)
+// include __DIR__ . 'menu.php';
+?>
+
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+
+</main>
+
+<?php
+// Include footer (chứa liên kết JavaScript)
+include __DIR__ . '\footer.php';
+?>
---
+++ admin/footer.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer>
+    <p>© 2024 Website Bán Vé Xem Phim. All rights reserved.</p>
+</footer>
+
+<!-- Liên kết đến các file JS dùng chung -->
+<script src="../assets/js/core/popper.min.js"></script>
+<script src="../assets/js/core/bootstrap.min.js"></script>
+<script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+<script src="../assets/js/plugins/chartjs.min.js"></script>
+<!-- Thêm các link JS khác nếu cần -->
+</body>
+
+</html>
