6a8cf30eb2683ed6299cdc53de097b046146b5a2
FOG <dn135897@gmail.com>
2024-10-19 10:38:25 +0700

    sửa links và navbar - 19/10/2024
    đem toàn bộ link ở header sang phần links.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -2,28 +2,30 @@
 <html lang="en">
 
 <head>
+    <?php
+    $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard'; // Mặc định là dashboard
+    switch ($page) {
+        case 'dashboard': {
+                $title = 'Dashboard';
+                break;
+            }
+        case 'films': {
+                $title = 'Danh sách phim';
+                break;
+            }
+        default: {
+                $title = 'Dashboard'; // Trường hợp mặc định nếu không có trang hợp lệ
+            }
+    }
+    ?>
     <!-- Required meta tags -->
+
+    <title><?php echo $title; ?></title>
     <meta charset="utf-8" />
     <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="shortcut icon" href="../assets/images/favicon-32x32.png" type="image/x-icon">
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossorigin="anonymous" />
+    <?php require('links.php'); ?>
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
---
+++ admin/includes/links.php
@@ -0,0 +1,17 @@
+<link rel="shortcut icon" href="../assets/images/favicon-32x32.png" type="image/x-icon">
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+<!-- Nucleo Icons -->
+<link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+<link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- Font Awesome Icons -->
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- CSS Files -->
+<link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+<!-- Bootstrap CSS v5.2.1 -->
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous" />
