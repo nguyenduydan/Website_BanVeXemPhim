0ca800161ec49a58e77a1ae41ad742b24d601446
FOG <dn135897@gmail.com>
2024-10-22 00:13:41 +0700

    sửa user -  22/101/2024

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -260,7 +260,7 @@
             <li class="nav-item">
                 <a class="nav-link <?php if ($current_page === "user-list.php") {
                                         echo "active";
-                                    } ?>" href="../admin/views/user/user-list.php" data-class="bg-white">
+                                    } ?>" href="../admin/user-list.php" data-class="bg-white">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1"
