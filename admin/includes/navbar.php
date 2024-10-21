98c69b0099efafc4ff6aa1e37ad9c69a6c1dc14e
FOG <dn135897@gmail.com>
2024-10-21 14:52:48 +0700

    chỉnh sửa navbar.php - 21/10/2024

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -42,12 +42,7 @@ $parent_title = $parent_pages[$current_page_name] ?? null;
                 </div>
             </div>
             <ul class="navbar-nav justify-content-end">
-                <li class="nav-item d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                        <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">Sign In</span>
-                    </a>
-                </li>
+
                 <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                         <div class="sidenav-toggler-inner">
