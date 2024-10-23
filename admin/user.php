611c3614e8907aed5e2414d6b5928f10b24207b2
luvuphuc <145564516+luvuphuc@users.noreply.github.com>
2024-10-23 11:46:42 +0700

    create user - 23/10/2024

--- admin/user.php
+++ admin/user.php
@@ -1,4 +1,7 @@
-<?php include('includes/header.php'); ?>
+<?php 
+    require '../config/function.php';
+    include('includes/header.php');
+?>
 
 <!-- Hiển thị nội dung danh sách phim -->
 <div class="row">
@@ -64,14 +67,15 @@
             </div>
             <div class="modal-body">
                 <div class="col-xl-12 col-lg-12 mx-auto">
-                    <form action="" method="POST">
+                    <?= alertMessage();?>
+                    <form action="code.php" method="post">
                         <div class="row">
                             <!-- Cột 1 -->
                             <div class="col-md-6">
                                 <!-- Nhập tên người dùng -->
                                 <div class="form-group mb-3">
-                                    <label for="ten_nguoi_dung">Tên người dùng</label>
-                                    <input type="text" class="form-control" id="ten_nguoi_dung" name="ten_nguoi_dung" placeholder="Nhập tên người dùng" required>
+                                    <label for="name">Tên người dùng</label>
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Nhập tên người dùng" required>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="password">Mật khẩu</label>
@@ -79,11 +83,11 @@
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="re_password">Nhập lại mật khẩu</label>
-                                    <input type="password" class="form-control" id="e_password" name="e_password" placeholder="Nhập lại mật khẩu" required>
+                                    <input type="password" class="form-control" id="re_password" name="re_password" placeholder="Nhập lại mật khẩu" required>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="ngay_sinh">Ngày sinh</label>
-                                    <input type="text" class="form-control" id="ngay_sinh" name="ngay_sinh" placeholder="Nhập ngày sinh" required>
+                                    <input type="date" class="form-control" id="ngay_sinh" name="ngay_sinh" placeholder="Nhập ngày sinh" required>
                                 </div>
                                 <!-- Dropdown giới tính-->
                                 <div class="form-group mb-3">
@@ -118,26 +122,18 @@
                                         <option value="Inactive">Offline</option>
                                     </select>
                                 </div>
-                                <div class="form-group mb-3">
-                                    <label for="anh_phim">Chọn ảnh phim</label>
-                                    <input type="file" class="form-control" id="anh_phim" name="anh_phim" accept="image/*" required onchange="previewImage(event)">
-                                </div>
+                                <!-- <div class="form-group mb-3">
+                                    <label for="avatar">Chọn ảnh</label>
+                                    <input type="file" class="form-control" id="avatar" name="avatar" accept="image/*" required onchange="previewImage(event)">
+                                </div> -->
                                 <!-- Hiển thị ảnh đã chọn -->
                                 <div class="form-group mb-3">
                                     <img id="preview" src="#" alt="Ảnh xem trước" style="display:none; max-width: 100%; height: auto;" />
                                 </div>
                             </div>
                         </div>
-
-
-                        <!-- Các trường tự động (Ẩn đi) -->
-                        <input type="hidden" id="ma_the_loai" name="ma_nguoi_dung" value="<?php echo uniqid(); ?>">
-                        <input type="hidden" id="nguoi_tao" name="nguoi_tao" value="<?php echo $_SESSION['username']; ?>">
-                        <input type="hidden" id="ngay_tao" name="ngay_tao" value="<?php echo date('Y-m-d H:i:s'); ?>">
-                        <input type="hidden" id="nguoi_cap_nhat" name="nguoi_cap_nhat" value="<?php echo $_SESSION['username']; ?>">
-                        <input type="hidden" id="ngay_cap_nhat" name="ngay_cap_nhat" value="<?php echo date('Y-m-d H:i:s'); ?>">
                         <!-- Nút submit -->
-                        <button type="submit" class="btn btn-primary">Lưu</button>
+                        <button type="submit" name="saveUser" class="btn btn-primary">Lưu</button>
                     </form>
                 </div>
             </div>
