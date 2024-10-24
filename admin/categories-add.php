0b0f34893231f6b89ca9a9d831b474fc0ad6a167
FOG <dn135897@gmail.com>
2024-10-24 16:53:33 +0700

    sửa categories-add

--- admin/categories-add.php
+++ admin/categories-add.php
@@ -10,19 +10,24 @@
             </a>
         </div>
         <form id="addCategoryForm" action="../admin/controllers/code.php" method="post">
-            <div class="row">
+            <div class="row ">
                 <!-- Cột 1 -->
-                <div class="col-md-12">
+                <div class="col-md-6 m-auto">
                     <!-- Nhập tên thể loại -->
                     <div class="form-group mb-3">
                         <label for="ten_the_loai">Tên thể loại</label>
-                        <input type="text" class="form-control" id="ten_the_loai" name="ten_the_loai" placeholder="Nhập tên thể loại" required>
+                        <input type="text" class="form-control" id="ten_the_loai" name="ten_the_loai" placeholder="Nhập tên thể loại">
                     </div>
+                    <div class="form-group mb-3">
+                        <label for="status">Trạng thái</label>
+                        <select class="form-control" id="status" name="status">
+                            <option value="1">Online</option>
+                            <option value="0">Offline</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-success w-15 mt-3">Lưu</button>
                 </div>
             </div>
-
-            <!-- Nút submit -->
-            <button type="submit" class="btn btn-success w-15 mt-3">Lưu</button>
         </form>
     </div>
 </div>
--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -46,7 +46,7 @@
 </style>
 
 <!-- Navbar -->
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none bg-white sticky-top" navbar-scroll="true">
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none sticky-top" style="background-color: #f8f9fa;" navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
