8a751ccc4765ef95a8bf278d31835c5ade27e1cb
FOG <dn135897@gmail.com>
2024-10-30 13:43:37 +0700

    update showtime

--- admin/showtime.php
+++ admin/showtime.php
@@ -30,7 +30,8 @@ $records_per_page = $pagination['records_per_page'];
                     </select>
                 </form>
                 <a href="views/showtime/showtime-add.php" class="btn btn-lg me-5 btn-add"
-                    style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: 20px; --bs-btn-font-size: 1.25rem;">Thêm</a>
+                    style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: 20px; --bs-btn-font-size: 1.25rem;">
+                    <i class="bi bi-plus me-1 fs-3" style="margin-bottom: 5px;"></i>Thêm</a>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
                 <div class="table-responsive p-0">
@@ -55,69 +56,69 @@ $records_per_page = $pagination['records_per_page'];
                                 foreach ($data as $item) {
                                     $stt++;
                             ?>
-                            <tr>
-                                <th class="text-center text-xs font-weight-bolder"><?= $stt ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['GioChieu']; ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['GioChieu']; ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['NguoiTao']; ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['NgayTao']; ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['NguoiCapNhat']; ?></th>
-                                <th class="text-center text-xs font-weight-bolder"><?= $item['NgayCapNhat']; ?></th>
-                                <th class="text-center text-s font-weight-bolder">
-                                    <form action="controllers/showtime-controller.php" method="POST"
-                                        style="display:inline;">
-                                        <input type="hidden" name="masuatchieu" value="<?= $item['MaSuatChieu'] ?>">
-                                        <input type="hidden" name="status"
-                                            value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
-                                        <button type="submit" name="changeStatus"
-                                            class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase"
-                                            style="border: none; cursor: pointer;">
-                                            <?= $item['TrangThai'] == 1 ? 'ON' : 'OFF' ?>
-                                        </button>
-                                    </form>
-                                </th>
-                                <td class="align-middle text-center text-sm">
-                                    <a class="btn btn-info m-0"
-                                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
-                                        href="views/showtime/showtime-edit.php?id=<?= $item['MaSuatChieu'] ?>">
-                                        <i class="bi bi-pencil"></i> Sửa
-                                    </a>
-                                    <a class="btn btn-danger m-0 delete-btn" data-id="<?= $item['MaSuatChieu'] ?>"
-                                        data-url="views/showtime/showtime-delete.php"
-                                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
-                                        data-bs-toggle="modal" data-bs-target="#confirmModal">
-                                        <i class="bi bi-trash"></i> Xoá
-                                    </a>
-                                    <div class="modal fade" id="confirmModal" tabindex="-1"
-                                        aria-labelledby="confirmModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog mt-10">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="confirmModalLabel">Xác Nhận Xóa</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <p class="p-2 fs-5">Bạn có muốn xóa không?</p>
-                                                </div>
-                                                <div class="modal-footer d-flex justify-content-center">
-                                                    <button type="button" id="confirmYes"
-                                                        class="btn btn-sm btn-success">Có</button>
-                                                    <button type="button" class="btn btn-sm btn-danger me-2"
-                                                        data-bs-dismiss="modal">Không</button>
+                                    <tr>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $stt ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['GioChieu']; ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['GioChieu']; ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['NguoiTao']; ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['NgayTao']; ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['NguoiCapNhat']; ?></th>
+                                        <th class="text-center text-xs font-weight-bolder"><?= $item['NgayCapNhat']; ?></th>
+                                        <th class="text-center text-s font-weight-bolder">
+                                            <form action="controllers/showtime-controller.php" method="POST"
+                                                style="display:inline;">
+                                                <input type="hidden" name="masuatchieu" value="<?= $item['MaSuatChieu'] ?>">
+                                                <input type="hidden" name="status"
+                                                    value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
+                                                <button type="submit" name="changeStatus"
+                                                    class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase"
+                                                    style="border: none; cursor: pointer;">
+                                                    <?= $item['TrangThai'] == 1 ? 'ON' : 'OFF' ?>
+                                                </button>
+                                            </form>
+                                        </th>
+                                        <td class="align-middle text-center text-sm">
+                                            <a class="btn btn-info m-0"
+                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                                href="views/showtime/showtime-edit.php?id=<?= $item['MaSuatChieu'] ?>">
+                                                <i class="bi bi-pencil"></i> Sửa
+                                            </a>
+                                            <a class="btn btn-danger m-0 delete-btn" data-id="<?= $item['MaSuatChieu'] ?>"
+                                                data-url="views/showtime/showtime-delete.php"
+                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                                data-bs-toggle="modal" data-bs-target="#confirmModal">
+                                                <i class="bi bi-trash"></i> Xoá
+                                            </a>
+                                            <div class="modal fade" id="confirmModal" tabindex="-1"
+                                                aria-labelledby="confirmModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog mt-10">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="confirmModalLabel">Xác Nhận Xóa</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <p class="p-2 fs-5">Bạn có muốn xóa không?</p>
+                                                        </div>
+                                                        <div class="modal-footer d-flex justify-content-center">
+                                                            <button type="button" id="confirmYes"
+                                                                class="btn btn-sm btn-success">Có</button>
+                                                            <button type="button" class="btn btn-sm btn-danger me-2"
+                                                                data-bs-dismiss="modal">Không</button>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            <?php
+                                        </td>
+                                    </tr>
+                                <?php
                                 }
                             } else {
                                 ?>
-                            <tr>
-                                <td colspan="10" class="text-center">Không có bản ghi nào</td>
-                            </tr>
+                                <tr>
+                                    <td colspan="10" class="text-center">Không có bản ghi nào</td>
+                                </tr>
                             <?php
                             }
                             ?>
