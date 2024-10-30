86eff3783fd358c0dd6d7acb4465a6fad430734f
FOG <dn135897@gmail.com>
2024-10-30 13:34:15 +0700

    Update room.php

--- admin/room.php
+++ admin/room.php
@@ -21,7 +21,8 @@ $records_per_page = $pagination['records_per_page'];
                 <h5><?php echo $title ?></h5>
                 <form method="POST" class="d-inline">
                     <label for="records_per_page" class="me-2 fs-6">Chọn hiển thị số bản ghi:</label>
-                    <select name="records_per_page" id="records_per_page" class="form-select" onchange="this.form.submit()">
+                    <select name="records_per_page" id="records_per_page" class="form-select"
+                        onchange="this.form.submit()">
                         <option value="2" <?= $records_per_page == 2 ? 'selected' : '' ?>>2</option>
                         <option value="5" <?= $records_per_page == 5 ? 'selected' : '' ?>>5</option>
                         <option value="10" <?= $records_per_page == 10 ? 'selected' : '' ?>>10</option>
@@ -64,37 +65,47 @@ $records_per_page = $pagination['records_per_page'];
                                         <th class="text-center text-xs font-weight-bolder"><?= $item['NguoiCapNhat']; ?></th>
                                         <th class="text-center text-xs font-weight-bolder"><?= $item['NgayCapNhat']; ?></th>
                                         <th class="text-center text-s font-weight-bolder">
-                                            <form action="controllers/room-controller.php" method="POST" style="display:inline;">
+                                            <form action="controllers/room-controller.php" method="POST"
+                                                style="display:inline;">
                                                 <input type="hidden" name="maphong" value="<?= $item['MaPhong'] ?>">
-                                                <input type="hidden" name="status" value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
-                                                <button type="submit" name="changeStatus" class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase" style="border: none; cursor: pointer;">
+                                                <input type="hidden" name="status"
+                                                    value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
+                                                <button type="submit" name="changeStatus"
+                                                    class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase"
+                                                    style="border: none; cursor: pointer;">
                                                     <?= $item['TrangThai'] == 1 ? 'ON' : 'OFF' ?>
                                                 </button>
                                             </form>
                                         </th>
                                         <td class="align-middle text-center text-sm">
-                                            <a class="btn btn-info m-0" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                            <a class="btn btn-info m-0"
+                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
                                                 href="views/room/room-edit.php?id=<?= $item['MaPhong'] ?>">
                                                 <i class="bi bi-pencil"></i> Sửa
                                             </a>
                                             <a class="btn btn-danger m-0 delete-btn" data-id="<?= $item['MaPhong'] ?>"
+                                                data-url="views/room/room-delete.php"
                                                 style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
                                                 data-bs-toggle="modal" data-bs-target="#confirmModal">
                                                 <i class="bi bi-trash"></i> Xoá
                                             </a>
-                                            <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+                                            <div class="modal fade" id="confirmModal" tabindex="-1"
+                                                aria-labelledby="confirmModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog mt-10">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
                                                             <h5 class="modal-title" id="confirmModalLabel">Xác Nhận Xóa</h5>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            <p class="p-2 fs-5">Bạn có muốn xóa người dùng này không?</p>
+                                                            <p class="p-2 fs-5">Bạn có muốn xóa không?</p>
                                                         </div>
                                                         <div class="modal-footer d-flex justify-content-center">
-                                                            <button type="button" class="btn btn-sm btn-success" id="confirmYes">Có</button>
-                                                            <button type="button" class="btn btn-sm btn-danger me-2" data-bs-dismiss="modal">Không</button>
+                                                            <button type="button" class="btn btn-sm btn-success"
+                                                                id="confirmYes">Có</button>
+                                                            <button type="button" class="btn btn-sm btn-danger me-2"
+                                                                data-bs-dismiss="modal">Không</button>
                                                         </div>
                                                     </div>
                                                 </div>
