c64193ea1f98ef0854ad18eb202ca557a4e4a33e
luvuphuc <145564516+luvuphuc@users.noreply.github.com>
2024-10-31 13:53:29 +0700

    film details done

--- admin/views/film/film-detail.php
+++ admin/views/film/film-detail.php
@@ -2,6 +2,12 @@
 
 require '../../../config/function.php';
 include('../../includes/header.php');
+$phanloai = [
+    0 => 'Phổ thông',
+    13 => 'T13',
+    16 => 'T16',
+    18 => 'T18'
+];
 ?>
 
 <div id="toast">
@@ -15,95 +21,105 @@ if (!is_numeric($id_result)) {
 $item = getByID('Phim', 'MaPhim', check_valid_ID('id'));
 if ($item['status'] == 200) {
 ?>
-<div class="row">
-    <div class="col-xl-12 col-lg-12 mx-auto">
-        <h2><?= $title ?></h2>
-        <div class="text-end mb-4">
-            <a class="btn btn-info" href="film-edit.php?id=<?= $id_result; ?>"><i class="bi bi-pencil me-2"></i>Sửa</a>
-            <a class="btn btn-secondary" href="../../film.php">
-                Quay lại
-            </a>
-        </div>
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 mx-auto">
+            <h2><?= $title ?></h2>
+            <div class="text-end mb-4">
+                <a class="btn btn-info" href="film-edit.php?id=<?= $id_result; ?>"><i class="bi bi-pencil me-2"></i>Sửa</a>
+                <a class="btn btn-secondary" href="../../film.php">
+                    Quay lại
+                </a>
+            </div>
 
-        <!-- Thông tin chi tiết phim -->
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <!-- Cột 1 -->
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="fs-6">Mã phim:</label>
-                            <span><?= $item['data']['MaPhim']; ?></span>
+            <!-- Thông tin chi tiết phim -->
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <!-- Cột 1 -->
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label class="fs-6">Mã phim:</label>
+                                <span><?= $item['data']['MaPhim']; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Tên phim:</label>
+                                <span><?= $item['data']['TenPhim']; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Thời lượng:</label>
+                                <span><?= $item['data']['ThoiLuong']; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Quốc gia:</label>
+                                <span><?= $item['data']['QuocGia'] ? $item['data']['QuocGia'] : 'Updating...'; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Đạo diễn:</label>
+                                <span><?= $item['data']['DaoDien'] ? $item['data']['DaoDien'] : 'Updating...'; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Diễn viên:</label>
+                                <span><?= $item['data']['DienVien'] ? $item['data']['DienVien'] : 'Updating...'; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Phân loại:</label>
+                                <span><?= $phanloai[$item['data']['PhanLoai']] ?? 'Chưa xác định'; ?></span>
+                            </div>
+                            <!-- Ảnh phim -->
+                            <div class="mb-3">
+                                <img id="preview"
+                                    src="<?php echo isset($item['data']['Anh']) ? '../../../uploads/film-imgs/' . htmlspecialchars($item['data']['Anh']) : '#'; ?>"
+                                    alt="Ảnh xem trước" class="img-fluid"
+                                    style="display: <?php echo isset($item['data']['Anh']) ? 'block' : 'none'; ?>; max-width: 100%; max-height: 150px;" />
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Tên phim:</label>
-                            <span><?= $item['data']['TenPhim']; ?></span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Thời lượng:</label>
-                            <span><?= $item['data']['ThoiLuong']; ?></span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Quốc gia:</label>
-                            <span><?= $item['data']['QuocGia'] ? $item['data']['QuocGia'] : 'Updating...'; ?></span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Đạo diễn:</label>
-                            <span><?= $item['data']['DaoDien'] ? $item['data']['DaoDien'] : 'Updating...'; ?></span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Diễn viên:</label>
-                            <span><?= $item['data']['DienVien'] ? $item['data']['DienVien'] : 'Updating...'; ?></span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Phân loại:</label>
-                            <span><?= $item['data']['PhanLoai'] ? $item['data']['PhanLoai'] : 'Updating...'; ?></span>
-                        </div>
-                        <!-- Ảnh phim -->
-                        <div class="mb-3">
-                            <img id="preview"
-                                src="<?php echo isset($item['data']['Anh']) ? '../../../uploads/film-imgs/' . htmlspecialchars($item['data']['Anh']) : '#'; ?>"
-                                alt="Ảnh xem trước" class="img-fluid"
-                                style="display: <?php echo isset($item['data']['Anh']) ? 'block' : 'none'; ?>; max-width: 100%; max-height: 150px;" />
+
+                        <!-- Cột 2 -->
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label class="fs-6">Thể loại:</label>
+                                <?php
+
+                                global $conn;
+                                $query = "SELECT GROUP_CONCAT(Theloai.TenTheLoai SEPARATOR ', ') AS TheLoai
+                                        FROM PHIM
+                                        JOIN THELOAI_FILM ON PHIM.MAPHIM = THELOAI_FILM.MAPHIM
+                                        JOIN THELOAI ON THELOAI_FILM.MATHELOAI = THELOAI.MATHELOAI
+                                        WHERE PHIM.MAPHIM = {$item['data']['MaPhim']}
+                                        GROUP BY PHIM.MAPHIM";
+                                $result = $conn->query($query);
+                                $genres = $result->fetch_assoc()['TheLoai'];
+                                $conn->close();
+                                ?>
+                                <span><?= $genres ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Người tạo:</label>
+                                <span>Nguyễn Văn E</span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Ngày tạo:</label>
+                                <span><?= isset($item['data']['NgayTao']) ? (new DateTime($item['data']['NgayTao']))->format('d/m/y H:i:s') : 'Chưa xác định'; ?></span>
+
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Người cập nhật:</label>
+                                <span>Trần Văn F</span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Ngày cập nhật:</label>
+                                <span><?= isset($item['data']['NgayCapNhat']) ? (new DateTime($item['data']['NgayCapNhat']))->format('d/m/y H:i:s') : 'Chưa xác định'; ?></span>
+                            </div>
+                            <div class="mb-3">
+                                <label class="fs-6">Mô tả:</label>
+                                <p><?= $item['data']['MoTa'] ?></p>
+                            </div>
                         </div>
                     </div>
-
-                    <!-- Cột 2 -->
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="fs-6">Thể loại:</label>
-                            <span>Hành động, Phiêu lưu</span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Người tạo:</label>
-                            <span>Nguyễn Văn E</span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Ngày tạo:</label>
-                            <span>01/01/2023</span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Người cập nhật:</label>
-                            <span>Trần Văn F</span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Ngày cập nhật:</label>
-                            <span>01/06/2023</span>
-                        </div>
-                        <div class="mb-3">
-                            <label class="fs-6">Mô tả:</label>
-                            <p>Phim nói về cuộc chiến giữa con người và quái vật trong một thế giới hậu tận thế.
-                                Phim nói về cuộc chiến giữa con người và quái vật trong một thế giới hậu tận thế.
-                                Phim nói về cuộc chiến giữa con người và quái vật trong một thế giới hậu tận thế.</p>
-                        </div>
-                    </div>
-
-
                 </div>
             </div>
         </div>
     </div>
-</div>
 <?php
 } else {
     echo '<h5>' . $item['message'] . '</h5>';
