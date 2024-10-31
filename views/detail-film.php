8fa240ef493314c02e61c494e4f10940f366ed05
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-10-31 13:48:52 +0700

    update detail film

--- views/detail-film.php
+++ views/detail-film.php
@@ -10,19 +10,17 @@
 
         <!-- Thông tin chi tiết phim -->
         <div class="col-lg-8">
-            <h2 class="movie-title">Thiên Đường Quả Báo <span class="movie-age">T18</span></h2>
+            <div class="d-flex align-items-center mb-2">
+                <h2 class="movie-title-detail me-3">Thiên Đường Quả Báo</h2>
+                <span class="movie-age-detail">T18</span>
+            </div>
             <div class="movie-meta">
                 <span class="me-3"><i class="bi bi-clock"></i> 131 Phút</span>
                 <span><i class="bi bi-calendar"></i> 30/10/2024</span>
             </div>
-            <div class="movie-rating my-3">
-                <span class="rating-star">&#9733; 9.1</span> <span class="rating-votes">(32 votes)</span>
-            </div>
-            <div class="movie-info">
+            <div class="movie-info mt-3">
                 <p><strong>Quốc gia:</strong> Thái Lan</p>
-                <p><strong>Nhà sản xuất:</strong> GDH 559, Jai Studios</p>
-                <p><strong>Thể loại:</strong> <span class="badge bg-secondary">Tâm Lý</span> <span
-                        class="badge bg-secondary">Giật Gân</span></p>
+                <p><strong>Thể loại:</strong> Hành động - mamma </p>
                 <p><strong>Đạo diễn:</strong> Boss Kuno</p>
                 <p><strong>Diễn viên:</strong> Jeff Satur, Engfa Waraha, Srida Puapimol</p>
             </div>
@@ -32,10 +30,10 @@
     <!-- Nội dung phim -->
     <div class="movie-content mt-5">
         <h4 class="fw-bold">Nội Dung Phim</h4>
-        <p>Thongkam (Jeff Satur) và Sek (Pongsakorn) làm lụng vất vả, cày ngày cày đêm để xây dựng một mái ấm. Sau bao
-            nỗ lực, cuối cùng họ cũng có quyền sở hữu trang trại sâu riêng ở Mae Hong Son...</p>
+        <p>Thongkam (Jeff Satur) và Sek (Pongsakorn) làm lụng vất vả, cày ngày cày đêm để xây dựng một mái ấm...</p>
     </div>
 </div>
+
 
 
 <?php include('../includes/footer.php'); ?>
