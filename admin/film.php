1b7e9ecd8bca32c90cc2323500ad2dd306ff1682
FOG <dn135897@gmail.com>
2024-10-31 12:19:34 +0700

    Update film.php

--- admin/film.php
+++ admin/film.php
@@ -58,86 +58,85 @@ $records_per_page = $pagination['records_per_page'];
                                 foreach ($data as $item) {
                                     $stt++;
                             ?>
-                                    <tr>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $stt ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['TenPhim']; ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['ThoiLuong']; ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['QuocGia']; ?></th>
-                                        <?php
+                            <tr>
+                                <th class="text-center text-xs font-weight-bolder"><?= $stt ?></th>
+                                <th class="text-center text-xs font-weight-bolder"><?= $item['TenPhim']; ?></th>
+                                <th class="text-center text-xs font-weight-bolder"><?= $item['ThoiLuong']; ?></th>
+                                <th class="text-center text-xs font-weight-bolder"><?= $item['QuocGia']; ?></th>
+                                <?php
                                             global $conn;
                                             $query = "SELECT GROUP_CONCAT(Theloai.TenTheLoai SEPARATOR ', ') AS TheLoai
                                                     FROM PHIM
                                                     JOIN THELOAI_FILM ON PHIM.MAPHIM = THELOAI_FILM.MAPHIM
                                                     JOIN THELOAI ON THELOAI_FILM.MATHELOAI = THELOAI.MATHELOAI
-                                                    WHERE PHIM.MAPHIM = {$item['MaPhim']} 
+                                                    WHERE PHIM.MAPHIM = {$item['MaPhim']}
                                                     GROUP BY PHIM.MAPHIM";
                                             $result = $conn->query($query);
                                             $genres = $result->fetch_assoc()['TheLoai'];
                                             $conn->close();
                                         ?>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $genres; ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['TenPhim']; ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['PhanLoai']; ?></th>
-                                        <th class="text-center text-xs font-weight-bolder"><?= $item['NgayTao']; ?></th>
-                                        <th class="text-center text-s font-weight-bolder">
-                                            <form action="controllers/film-controller.php" method="POST"
-                                                style="display:inline;">
-                                                <input type="hidden" name="ma_phim" value="<?= $item['MaPhim'] ?>">
-                                                <input type="hidden" name="status"
-                                                    value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
-                                                <button type="submit" name="changeStatus"
-                                                    class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase"
-                                                    style="border: none; cursor: pointer;">
-                                                    <?= $item['TrangThai'] == 1 ? 'ON' : 'OFF' ?>
-                                                </button>
-                                            </form>
-                                        </th>
-                                        <td class="align-middle text-center text-sm">
-                                            <a class="btn btn-secondary m-0"
-                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
-                                                href="views/film/film-detail.php?id=<?= $item['MaPhim'] ?>">
-                                                <i class="bi bi-info-circle"></i> Chi tiết
-                                            </a>
-                                            <a class="btn btn-info m-0"
-                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
-                                                href="views/film/film-edit.php?id=<?= $item['MaPhim'] ?>">
-                                                <i class="bi bi-pencil"></i> Sửa
-                                            </a>
-                                            <a class="btn btn-danger m-0 delete-btn" data-id="<?= $item['MaPhim'] ?>"
-                                                style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
-                                                data-bs-toggle="modal" data-bs-target="#confirmModal">
-                                                <i class="bi bi-trash"></i> Xoá
-                                            </a>
-                                            <div class="modal fade" id="confirmModal" tabindex="-1"
-                                                aria-labelledby="confirmModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog mt-10">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="confirmModalLabel">Xác Nhận Xóa</h5>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                                aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            <p class="p-2 fs-5">Bạn có muốn xóa người dùng này không?</p>
-                                                        </div>
-                                                        <div class="modal-footer d-flex justify-content-center">
-                                                            <button type="button" class="btn btn-sm btn-success"
-                                                                id="confirmYes">Có</button>
-                                                            <button type="button" class="btn btn-sm btn-danger me-2"
-                                                                data-bs-dismiss="modal">Không</button>
-                                                        </div>
-                                                    </div>
+                                <th class="text-center text-xs font-weight-bolder"><?= $genres; ?></th>
+                                <th class="text-center text-xs font-weight-bolder"><?= $item['PhanLoai']; ?></th>
+                                <th class="text-center text-xs font-weight-bolder"><?= $item['NgayTao']; ?></th>
+                                <th class="text-center text-s font-weight-bolder">
+                                    <form action="controllers/film-controller.php" method="POST"
+                                        style="display:inline;">
+                                        <input type="hidden" name="ma_phim" value="<?= $item['MaPhim'] ?>">
+                                        <input type="hidden" name="status"
+                                            value="<?= $item['TrangThai'] == 1 ? 0 : 1 ?>">
+                                        <button type="submit" name="changeStatus"
+                                            class="badge badge-sm <?= $item['TrangThai'] == 1 ? 'bg-gradient-success' : 'bg-gradient-secondary' ?> text-uppercase"
+                                            style="border: none; cursor: pointer;">
+                                            <?= $item['TrangThai'] == 1 ? 'ON' : 'OFF' ?>
+                                        </button>
+                                    </form>
+                                </th>
+                                <td class="align-middle text-center text-sm">
+                                    <a class="btn btn-secondary m-0"
+                                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                        href="views/film/film-detail.php?id=<?= $item['MaPhim'] ?>">
+                                        <i class="bi bi-info-circle"></i> Chi tiết
+                                    </a>
+                                    <a class="btn btn-info m-0"
+                                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                        href="views/film/film-edit.php?id=<?= $item['MaPhim'] ?>">
+                                        <i class="bi bi-pencil"></i> Sửa
+                                    </a>
+                                    <a class="btn btn-danger m-0 delete-btn" data-id="<?= $item['MaPhim'] ?>"
+                                        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"
+                                        data-bs-toggle="modal" data-bs-target="#confirmModal">
+                                        <i class="bi bi-trash"></i> Xoá
+                                    </a>
+                                    <div class="modal fade" id="confirmModal" tabindex="-1"
+                                        aria-labelledby="confirmModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog mt-10">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="confirmModalLabel">Xác Nhận Xóa</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p class="p-2 fs-5">Bạn có muốn xóa người dùng này không?</p>
+                                                </div>
+                                                <div class="modal-footer d-flex justify-content-center">
+                                                    <button type="button" class="btn btn-sm btn-success"
+                                                        id="confirmYes">Có</button>
+                                                    <button type="button" class="btn btn-sm btn-danger me-2"
+                                                        data-bs-dismiss="modal">Không</button>
                                                 </div>
                                             </div>
-                                        </td>
-                                    </tr>
-                                <?php
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
                                 }
                             } else {
                                 ?>
-                                <tr>
-                                    <td colspan="10" class="text-center">Không có bản ghi nào</td>
-                                </tr>
+                            <tr>
+                                <td colspan="10" class="text-center">Không có bản ghi nào</td>
+                            </tr>
                             <?php
                             }
                             ?>
