d7fb7c9c17337de0e132d2c46d9ba71ea99a40e1
FOG <dn135897@gmail.com>
2024-11-02 17:17:59 +0700

    Update sidebar.php

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -97,10 +97,12 @@
 
             <li class="nav-item dropdown">
                 <a class="nav-link ps-4 ms-2 text-uppercase text-xs font-weight-bolder dropdown-toggle"
-                    data-bs-toggle="collapse" data-bs-target="#listFilm" aria-expanded="false">
+                    data-bs-toggle="collapse" data-bs-target="#listFilm"
+                    aria-expanded="<?php echo (isActive($current_page, $base_pages, 'categories') || isActive($current_page, $base_pages, 'film') || isActive($current_page, $base_pages, 'room') || isActive($current_page, $base_pages, 'chair') || isActive($current_page, $base_pages, 'showtime')) ? 'true' : 'false'; ?>">
                     <span class="nav-link-text ms-1">Danh sách phim</span>
                 </a>
-                <ul class="nav nav-treeview collapse" id="listFilm">
+                <ul class="nav nav-treeview <?php echo (isActive($current_page, $base_pages, 'categories') || isActive($current_page, $base_pages, 'film') || isActive($current_page, $base_pages, 'room') || isActive($current_page, $base_pages, 'chair') || isActive($current_page, $base_pages, 'showtime')) ? 'show' : 'collapse'; ?>"
+                    id="listFilm">
                     <li class="nav-item">
                         <a class="nav-link <?php if (isActive($current_page, $base_pages, 'categories')) echo 'active'; ?>"
                             href="/Website_BanVeXemPhim/admin/categories.php" data-class="bg-white">
@@ -121,9 +123,8 @@
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                                 <svg width="12px" height="12px" viewBox="0 0 16 16" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g>
-
                                         <path class="color-background opacity-6"
                                             d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
                                     </g>
@@ -152,12 +153,11 @@
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                                 <svg width="12px" height="12px" viewBox="0 0 640 512" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g>
                                         <path
                                             d="M64 160C64 89.3 121.3 32 192 32l256 0c70.7 0 128 57.3 128 128l0 33.6c-36.5 7.4-64 39.7-64 78.4l0 48-384 0 0-48c0-38.7-27.5-71-64-78.4L64 160zM544 272c0-20.9 13.4-38.7 32-45.3c5-1.8 10.4-2.7 16-2.7c26.5 0 48 21.5 48 48l0 176c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32L96 448c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32L0 272c0-26.5 21.5-48 48-48c5.6 0 11 1 16 2.7c18.6 6.6 32 24.4 32 45.3l0 48 0 32 32 0 384 0 32 0 0-32 0-48z">
                                         </path>
-
                                     </g>
                                 </svg>
                             </div>
@@ -170,16 +170,14 @@
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                                 <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g>
-
                                         <path
                                             d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
                                         </path>
                                         <path
-                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
+                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 Z">
                                         </path>
-
                                     </g>
                                 </svg>
                             </div>
@@ -191,24 +189,25 @@
 
             <li class="nav-item dropdown">
                 <a class="nav-link ps-4 ms-2 text-uppercase text-xs font-weight-bolder dropdown-toggle"
-                    data-bs-toggle="collapse" data-bs-target="#listContent" aria-expanded="false">
+                    data-bs-toggle="collapse" data-bs-target="#listContent"
+                    aria-expanded="<?php echo (isActive($current_page, $base_pages, 'topic') || isActive($current_page, $base_pages, 'content')) ? 'true' : 'false'; ?>">
                     <span class="nav-link-text ms-1">Danh sách nội dung</span>
                 </a>
 
-                <ul class="nav nav-treeview collapse" id="listContent">
+                <ul class="nav nav-treeview <?php echo (isActive($current_page, $base_pages, 'topic') || isActive($current_page, $base_pages, 'content')) ? 'show' : 'collapse'; ?>"
+                    id="listContent">
                     <li class="nav-item">
                         <a class="nav-link <?php if (isActive($current_page, $base_pages, 'topic')) echo 'active'; ?>"
                             href="/Website_BanVeXemPhim/admin/topic.php" data-class="bg-white">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                                <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <svg width="12px" height="12px" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
                                     <g>
                                         <path class="color-background opacity-6"
                                             d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
                                         </path>
                                         <path class="color-background"
-                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
+                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 Z">
                                         </path>
                                     </g>
                                 </svg>
@@ -221,14 +220,13 @@
                             href="/Website_BanVeXemPhim/admin/content.php" data-class="bg-white">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                                <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <svg width="12px" height="12px" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
                                     <g>
                                         <path class="color-background opacity-6"
                                             d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
                                         </path>
                                         <path class="color-background"
-                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
+                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 Z">
                                         </path>
                                     </g>
                                 </svg>
@@ -241,23 +239,24 @@
 
             <li class="nav-item dropdown">
                 <a class="nav-link ps-4 ms-2 text-uppercase text-xs font-weight-bolder dropdown-toggle"
-                    data-bs-toggle="collapse" data-bs-target="#listmenu" aria-expanded="false">
+                    data-bs-toggle="collapse" data-bs-target="#listmenu"
+                    aria-expanded="<?php echo (isActive($current_page, $base_pages, 'slider') || isActive($current_page, $base_pages, 'menu')) ? 'true' : 'false'; ?>">
                     <span class="nav-link-text ms-1">Danh sách menu</span>
                 </a>
-                <ul class="nav nav-treeview collapse" id="listmenu">
+                <ul class="nav nav-treeview <?php echo (isActive($current_page, $base_pages, 'slider') || isActive($current_page, $base_pages, 'menu')) ? 'show' : 'collapse'; ?>"
+                    id="listmenu">
                     <li class="nav-item">
                         <a class="nav-link <?php if (isActive($current_page, $base_pages, 'slider')) echo 'active'; ?>"
                             href="/Website_BanVeXemPhim/admin/slider.php" data-class="bg-white">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                                <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <svg width="12px" height="12px" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
                                     <g>
                                         <path class="color-background opacity-6"
                                             d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
                                         </path>
                                         <path class="color-background"
-                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
+                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 Z">
                                         </path>
                                     </g>
                                 </svg>
@@ -270,14 +269,13 @@
                             href="/Website_BanVeXemPhim/admin/menu.php" data-class="bg-white">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                                <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <svg width="12px" height="12px" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
                                     <g>
                                         <path class="color-background opacity-6"
                                             d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
                                         </path>
                                         <path class="color-background"
-                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
+                                            d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 Z">
                                         </path>
                                     </g>
                                 </svg>
