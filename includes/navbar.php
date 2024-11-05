a430c26df9f713c83b4cfbd8c1b61ae7c19b9f55
FOG <dn135897@gmail.com>
2024-11-05 12:11:57 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,34 +1,31 @@
 <?php
-$current_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; // Hoặc $_SERVER['PHP_SELF']
+$current_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.php";
 ?>
 
 <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light navbar-blur p-3">
     <div class="container-fluid mx-5">
         <div class="container">
-            <div
-                class="d-flex row flex-wrap flex-lg-nowrap align-items-center justify-content-center justify-content-lg-center">
+            <div class="d-flex row flex-lg-nowrap align-items-center justify-content-center justify-content-lg-center">
+                <!-- Logo -->
                 <div
-                    class="col-lg-1 d-none d-lg-block col text-center justify-content-center col-lg-auto me-lg-auto mb-md-0">
+                    class="col-lg-1 d-block col-2 d-lg-block text-center justify-content-center col-lg-auto me-lg-auto mb-md-0">
                     <a href="http://localhost/Website_BanVeXemPhim/index.php" class="me-5">
                         <img src="/Website_BanVeXemPhim/assets/imgs/logo-100x100.png" style="width: 60px;"
                             class="bg-dark rounded-circle">
                     </a>
                 </div>
 
-                <div class="col-lg-7 d-flex flex-column align-items-start align-items-lg-center">
-                    <!-- Nút điều hướng cho thiết bị di động -->
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
 
-                    <?php
-                    $items = getAll('Menu');
-                    ?>
-
-                    <!-- Phần collapse chứa menu -->
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                <!-- Offcanvas sidebar cho menu trên màn hình nhỏ -->
+                <div class="offcanvas col-sm-4 offcanvas-end w-25" tabindex="-1" id="offcanvasMenu"
+                    aria-labelledby="offcanvasMenuLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <?php $items = getAll('Menu'); ?>
                         <ul
                             class="nav flex-lg-row flex-column col-lg-12 col-sm-12 me-lg-auto mb-2 justify-content-start justify-content-lg-center mb-md-0">
                             <?php foreach ($items as $item): ?>
@@ -43,18 +40,15 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.
                     </div>
                 </div>
 
-                <!-- Phần tìm kiếm sẽ nằm ở đây -->
+                <!-- Phần tìm kiếm và dropdown người dùng -->
                 <div
-                    class="col-12 mt-lg-0 mt-2 col-lg-4 d-flex justify-content-start justify-content-sm-start flex-column">
+                    class="col-8 mt-lg-0 mt-2 col-lg-4 d-flex justify-content-start justify-content-sm-start flex-column">
                     <div class="d-flex">
-                        <!-- Form tìm kiếm -->
                         <form class="mb-3 mb-lg-0 me-3 input-group w-100 flex-nowrap" role="search">
                             <span class="input-group-text bg-dark text-white border" style="cursor: pointer;"
                                 id="addon-wrapping"><i class="bi bi-search"></i></span>
                             <input type="search" class="form-control ps-4" placeholder="Search..." aria-label="Search">
                         </form>
-
-                        <!-- Dropdown người dùng -->
                         <div class="dropdown col-1 col-lg-2 text-end">
                             <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
                                 data-bs-toggle="dropdown" aria-expanded="false">
@@ -72,6 +66,13 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.
                             </ul>
                         </div>
                     </div>
+                </div>
+                <!-- Nút mở sidebar trên màn hình nhỏ -->
+                <div class="col-1 d-lg-none ms-4 ms-lg-0">
+                    <button class="btn btn-dark" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+                        <i class="bi bi-list"></i>
+                    </button>
                 </div>
             </div>
         </div>
