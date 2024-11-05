d00dd10d9efb1e3ce67974b2460f76465647e30b
FOG <dn135897@gmail.com>
2024-11-06 00:27:51 +0700

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -3,11 +3,4 @@ require_once '../config/function.php';
 session_start();
 $_SESSION['loggedIn'] = false;
 
-// Hủy tất cả dữ liệu trong session
-session_unset();
-
-// Hủy session
-session_destroy();
-
-
 redirect('sign-in.php', 'success', 'Đăng xuất thành công');
