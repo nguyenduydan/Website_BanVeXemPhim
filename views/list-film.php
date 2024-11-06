3ee2fc755cb60672a33e8d77bdb0c38eadff9c5d
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-11-06 10:48:32 +0700

    Update list-film.php

--- views/list-film.php
+++ views/list-film.php
@@ -31,7 +31,7 @@ require_once("config/function.php");
                     <div class="movie-card card">
                         <img class="img-fluid" src="uploads/film-imgs/<?= $item['Anh'] ?>"
                             alt="<?= $item['TenPhim'] ?>">
-                        <span class="movie-age">T<?= $item['PhanLoai'] ?></span>
+                        <span class="movie-age"><?= $item['PhanLoai'] ?></span>
                         <a href="views/detail-film.php?id=<?= $item['MaPhim'] ?>" class="buy-ticket">
                             <i class="bi bi-ticket-perforated"></i> Mua Vé
                         </a>
@@ -54,7 +54,7 @@ require_once("config/function.php");
                     <div class="movie-card card">
                         <img class="img-fluid" src="uploads/film-imgs/<?= $item['Anh'] ?>"
                             alt="<?= $item['TenPhim'] ?>">
-                        <span class="movie-age">T<?= $item['PhanLoai'] ?></span>
+                        <span class="movie-age"><?= $item['PhanLoai'] ?></span>
                     </div>
                     <div class="movie-info">
                         <div class="movie-title"><?= $item['TenPhim'] ?></div>
