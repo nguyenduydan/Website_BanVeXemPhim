b318b6bbb911ecb5d99675145b61d1a4c4c56951
FOG <dn135897@gmail.com>
2024-11-07 12:53:48 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -43,12 +43,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.
                             class="nav flex-lg-row flex-column col-lg-12 col-sm-12 me-lg-auto mb-2 justify-content-start justify-content-lg-center mb-md-0">
                             <?php foreach ($items as $item):
                             ?>
-                                <li class="nav-item mx-2">
-                                    <a href="<?= $baseUrl . $item['LienKet'] ?>"
-                                        class="nav-link px-2 fw-bolder text-capitalize text-secondary <?= ($current_url === $baseUrl . $item['LienKet']) ? 'active' : '' ?>">
-                                        <?= htmlspecialchars($item['TenMenu']) ?>
-                                    </a>
-                                </li>
+                            <li class="nav-item mx-2">
+                                <a href="<?= $baseUrl . $item['LienKet'] ?>"
+                                    class="nav-link px-2 fw-bolder text-capitalize text-secondary <?= ($current_url === $baseUrl . $item['LienKet']) ? 'active' : '' ?>">
+                                    <?= htmlspecialchars($item['TenMenu']) ?>
+                                </a>
+                            </li>
                             <?php endforeach; ?>
                         </ul>
                     </div>
@@ -72,8 +72,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.
                             <ul class="dropdown-menu text-small shadow border-0">
                                 <li class="dropdown-css">
                                     <a class="dropdown-item">
-                                        <img src="assets/imgs/wave.gif" class="bg-transparent" width="25px"
-                                            height="25px" alt="">
+                                        <img src="<?= $baseUrl ?>/assets/imgs/wave.gif" class="bg-transparent"
+                                            width="25px" height="25px" alt="">
                                         <span>Xin chào, </span>
                                         <span class="fw-bold user-name" style="
                                         background: linear-gradient(to right, #30CFD0 0%, #330867 100%);
@@ -114,58 +114,58 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Website_BanVeXemPhim/config/function.
     </div>
 </nav>
 <script>
-    var words = document.getElementsByClassName('word');
-    var wordArray = [];
-    var currentWord = 0;
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
 
-    words[currentWord].style.opacity = 1;
-    for (var i = 0; i < words.length; i++) {
-        splitLetters(words[i]);
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+    splitLetters(words[i]);
+}
+
+function changeWord() {
+    var cw = wordArray[currentWord];
+    var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+    for (var i = 0; i < cw.length; i++) {
+        animateLetterOut(cw, i);
     }
 
-    function changeWord() {
-        var cw = wordArray[currentWord];
-        var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-        for (var i = 0; i < cw.length; i++) {
-            animateLetterOut(cw, i);
-        }
-
-        for (var i = 0; i < nw.length; i++) {
-            nw[i].className = 'letter behind';
-            nw[0].parentElement.style.opacity = 1;
-            animateLetterIn(nw, i);
-        }
-
-        currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+    for (var i = 0; i < nw.length; i++) {
+        nw[i].className = 'letter behind';
+        nw[0].parentElement.style.opacity = 1;
+        animateLetterIn(nw, i);
     }
 
-    function animateLetterOut(cw, i) {
-        setTimeout(function() {
-            cw[i].className = 'letter out';
-        }, i * 80);
+    currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+    setTimeout(function() {
+        cw[i].className = 'letter out';
+    }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+    setTimeout(function() {
+        nw[i].className = 'letter in';
+    }, 340 + (i * 80));
+}
+
+function splitLetters(word) {
+    var content = word.innerHTML;
+    word.innerHTML = '';
+    var letters = [];
+    for (var i = 0; i < content.length; i++) {
+        var letter = document.createElement('span');
+        letter.className = 'letter';
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
     }
 
-    function animateLetterIn(nw, i) {
-        setTimeout(function() {
-            nw[i].className = 'letter in';
-        }, 340 + (i * 80));
-    }
+    wordArray.push(letters);
+}
 
-    function splitLetters(word) {
-        var content = word.innerHTML;
-        word.innerHTML = '';
-        var letters = [];
-        for (var i = 0; i < content.length; i++) {
-            var letter = document.createElement('span');
-            letter.className = 'letter';
-            letter.innerHTML = content.charAt(i);
-            word.appendChild(letter);
-            letters.push(letter);
-        }
-
-        wordArray.push(letters);
-    }
-
-    changeWord();
-    setInterval(changeWord, 4000);
+changeWord();
+setInterval(changeWord, 4000);
 </script>
