1d273fcacb1cf598675f7ffbf2fcb39b2ac5fd22
FOG <dn135897@gmail.com>
2024-11-08 20:39:47 +0700

    update error page

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php include('includes/header.php'); ?>
-<div class="d-flex align-items-center justify-content-center vh-100">
+<div class="d-flex align-items-center justify-content-center vh-50">
     <div class="text-center row">
-        <div class=" col-md-6">
-            <img src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg" alt="" class="img-fluid">
+        <div class=" col-md-7">
+            <img src="/Website_BanVeXemPhim/assets/imgs/404.gif" alt="" class="img-fluid">
         </div>
-        <div class=" col-md-6 mt-5">
-            <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+        <div class=" col-md-5 mt-5 py-5">
+            <p class="fs-3 mt-5"> <span class="text-danger">Opps!</span> Page not found.</p>
             <p class="lead">
                 The page you’re looking for doesn’t exist.
             </p>
