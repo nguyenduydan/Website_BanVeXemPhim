80cd1e76765f0075a029b8ab968dc35fc3319387
FOG <dn135897@gmail.com>
2024-11-09 13:56:59 +0700

    Update sendmail.php

--- config/sendmail.php
+++ config/sendmail.php
@@ -7,11 +7,12 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 require 'function.php';
+
 // Tạo một instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    // Enable verbose debug output
+    // Thiết lập charset UTF-8
     $mail->CharSet = 'UTF-8';
     $mail->isSMTP();                                            // Gửi bằng SMTP
     $mail->Host       = 'smtp.gmail.com';                     // Đặt máy chủ SMTP
@@ -21,25 +22,25 @@ try {
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        // Kích hoạt mã hóa TLS
     $mail->Port       = 587;                                    // Cổng TCP để kết nối
 
-    // Người gửi (email của khách hàng)
-    $customerEmail = $_POST['email']; // Địa chỉ email của khách hàng
+    // Người gửi (email của admin)
     $mail->setFrom('dn135897@gmail.com', 'NguyenThietDuyDan'); // Đặt địa chỉ email của admin làm người gửi
 
-    // Thêm header để cho biết email đến từ khách hàng
+    // Thêm địa chỉ email của khách hàng vào Reply-To
+    $customerEmail = $_POST['email']; // Địa chỉ email của khách hàng
     $mail->addReplyTo($customerEmail, $_POST['fullname']); // Địa chỉ email của khách hàng làm địa chỉ phản hồi
 
     // Người nhận (email của admin)
-    $mail->addAddress($customerEmail, $_POST['fullname']); // Địa chỉ email của admin
+    $mail->addAddress('dn135897@gmail.com', 'NguyenThietDuyDan'); // Địa chỉ email của admin
 
     // Nội dung email
     $mail->isHTML(true);                                        // Đặt định dạng email là HTML
     $mail->Subject = $_POST['subject'];                        // Tiêu đề email
-    $mail->Body    = '<h2>' . htmlspecialchars($_POST['message']) . '</h2> </br> <h2>' . htmlspecialchars($_POST['phone']) . '</h2>'; // Nội dung email
+    $mail->Body    = '<h2>' . htmlspecialchars($_POST['message']) . '</h2><br><h2>' . htmlspecialchars($_POST['phone']) . '</h2>'; // Nội dung email
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients'; // Nội dung plain-text
 
     $mail->send();
     redirect('contact.php', 'success', 'Gửi Email thành công');
 } catch (Exception $e) {
     $error = $mail->ErrorInfo;
-    redirect('contact.php', 'success', 'Tin nhắn không thể gửi đi. Lỗi: ' . $error);
+    redirect('contact.php', 'error', 'Tin nhắn không thể gửi đi. Lỗi: ' . $error);
 }
