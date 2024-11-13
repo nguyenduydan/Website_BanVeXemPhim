e6b5ea532cecd9bbefde89f393de6961b08d3c4f
FOG <dn135897@gmail.com>
2024-11-13 23:34:06 +0700

    update card rank

--- profile-user.php
+++ profile-user.php
@@ -49,19 +49,27 @@ getUser();
 
             // Determine membership level and corresponding styles
             if ($client_revenue < $silverValue) {
+                //rank none
                 $level = "Silver";
+                $color = "#d6d6e7";
                 $backround = "bg-member-none";
                 $diff = $silverValue - $client_revenue;
             } elseif ($client_revenue < $goldValue) {
+                //rank silver
                 $level = "Gold";
+                $color = "#6c757d";
                 $backround = "bg-member-silver";
                 $diff = $goldValue - $client_revenue;
             } elseif ($client_revenue < $platinumValue) {
+                //rank gold
                 $level = "Platinum";
+                $color = "#ffc107";
                 $backround = "bg-member-gold";
                 $diff = $platinumValue - $client_revenue;
             } else {
+                //rank platinum
                 $level = "Đã max";
+                $color = "#1948ff";
                 $backround = "bg-member-platinum";
                 $diff = 0; // Đã đạt tối đa
             }
@@ -73,53 +81,53 @@ getUser();
 
             ?>
             <style>
-            .bg-member-gold {
-                color: aliceblue;
-                /* Một màu vàng nhạt hơn */
-                background-image: url(https://i.pinimg.com/736x/9d/5c/13/9d5c13e3d92af6e99120a7b2394263cf.jpg);
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center;
-                border: 3px solid gold;
-                box-shadow: 0 0 10px 3px gold;
+                .bg-member-gold {
+                    color: aliceblue;
+                    /* Một màu vàng nhạt hơn */
+                    background-image: url(https://i.pinimg.com/736x/9d/5c/13/9d5c13e3d92af6e99120a7b2394263cf.jpg);
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    background-position: center;
+                    border: 3px solid gold;
+                    box-shadow: 0 0 10px 3px gold;
 
-            }
+                }
 
-            .bg-member-silver {
-                color: aliceblue;
-                /* Một màu vàng nhạt hơn */
-                background-image: url(https://i.pinimg.com/736x/2d/a9/e8/2da9e8590c45ef52fd0ce61a822a8197.jpg);
-                background-repeat: no-repeat;
-                background-size: cover;
-                box-shadow: 0 0 10px 3px silver;
-                background-position: center;
-                border: 3px solid silver;
-            }
+                .bg-member-silver {
+                    color: aliceblue;
+                    /* Một màu vàng nhạt hơn */
+                    background-image: url(https://i.pinimg.com/736x/2d/a9/e8/2da9e8590c45ef52fd0ce61a822a8197.jpg);
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    box-shadow: 0 0 10px 3px silver;
+                    background-position: center;
+                    border: 3px solid silver;
+                }
 
 
-            .bg-member-platinum {
-                color: aliceblue;
-                /* Một màu vàng nhạt hơn */
-                background-image: url(https://media4.giphy.com/media/xTiTnxpQ3ghPiB2Hp6/giphy.gif?cid=6c09b95219sdvq77x4l0mzc1omfnw4yceualg0y556obpxif&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g);
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center;
-                border: 3px solid dodgerblue;
-                box-shadow: 0 0 10px 3px dodgerblue;
-            }
+                .bg-member-platinum {
+                    color: aliceblue;
+                    /* Một màu vàng nhạt hơn */
+                    background-image: url(https://media4.giphy.com/media/xTiTnxpQ3ghPiB2Hp6/giphy.gif?cid=6c09b95219sdvq77x4l0mzc1omfnw4yceualg0y556obpxif&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g);
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    background-position: center;
+                    border: 3px solid dodgerblue;
+                    box-shadow: 0 0 10px 3px dodgerblue;
+                }
 
-            .bg-member-none {
-                color: aliceblue;
-                /* Một màu vàng nhạt hơn */
-                background-image: url(https://i.pinimg.com/736x/17/a9/a8/17a9a801d17a79fbafc52e4e19aa8750.jpg);
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center;
-                border: 3px solid black;
-                box-shadow: 0 0 10px 3px lightslategray;
-            }
+                .bg-member-none {
+                    color: aliceblue;
+                    /* Một màu vàng nhạt hơn */
+                    background-image: url(https://i.pinimg.com/736x/17/a9/a8/17a9a801d17a79fbafc52e4e19aa8750.jpg);
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    background-position: center;
+                    border: 3px solid black;
+                    box-shadow: 0 0 10px 3px lightslategray;
+                }
 
-            ;
+                ;
             </style>
 
             <div class="col-md-4 col-lg-5 mb-4">
@@ -128,9 +136,9 @@ getUser();
                         <div class="profile-picture-container position-relative d-inline-block">
                             <input type="file" class="form-control d-none" id="avatar" name="avatar" accept="image/*"
                                 onchange="submitAvatarForm();">
-                            <img id="preview"
+                            <img id="preview" style="box-shadow: 0 0 10px 5px <?= $color; ?>;"
                                 src="<?= $baseUrl . 'uploads/avatars/' . (!empty($user['data']['Anh']) ? $user['data']['Anh'] : 'user-icon.png') ?>"
-                                class="rounded-circle border border-2 border-light shadow-sm mb-3" alt="Profile Picture"
+                                class="rounded-circle border border-2 border-light mb-3" alt="Profile Picture"
                                 width="120" height="120">
                             <button id="camera" type="button"
                                 class="position-absolute top-50 start-50 translate-middle text-white d-none"
@@ -140,48 +148,49 @@ getUser();
                         </div>
 
                         <script>
-                        document.getElementById('camera').addEventListener('click', function(event) {
-                            event.preventDefault();
-                            document.getElementById('avatar').click();
-                        });
+                            document.getElementById('camera').addEventListener('click', function(event) {
+                                event.preventDefault();
+                                document.getElementById('avatar').click();
+                            });
 
-                        function submitAvatarForm() {
-                            var submitButton = document.createElement('input');
-                            submitButton.type = 'hidden';
-                            submitButton.name = 'updateAvt';
-                            document.getElementById('avatarForm').appendChild(submitButton);
-                            document.getElementById('avatarForm').submit();
-                        }
+                            function submitAvatarForm() {
+                                var submitButton = document.createElement('input');
+                                submitButton.type = 'hidden';
+                                submitButton.name = 'updateAvt';
+                                document.getElementById('avatarForm').appendChild(submitButton);
+                                document.getElementById('avatarForm').submit();
+                            }
                         </script>
 
                         <style>
-                        .profile-picture-container:hover #camera {
-                            display: flex !important;
-                            align-items: center;
-                            justify-content: center;
-                        }
+                            .profile-picture-container:hover #camera {
+                                display: flex !important;
+                                align-items: center;
+                                justify-content: center;
+                            }
                         </style>
 
-                        <h5 class="fw-bold mb-3">
+                        <h5 class="fw-bold mt-3 mb-2">
                             <i class="<?= $icon ?> me-2"></i>
                             <?= $user['data']['TenND'] ?>
                         </h5>
-                        <h5 class="mt-3">Tổng chi tiêu <?= $current_year = date('Y'); ?></h5>
-                        <p class="fw-bold"><?= number_format($client_revenue, 0, ',', '.') ?> ₫</p>
+                        <small class="mt-3">Tổng chi tiêu <span
+                                class="text-info fw-bolder fs-5 text-decoration-underline"><?= $current_year = date('Y'); ?></span></small>
+                        <p class="fw-bold fs-4"><?= number_format($client_revenue, 0, ',', '.') ?> ₫</p>
 
-                        <div class="progress my-3" style="height: 10px; position: relative;">
+                        <div class="progress my-3" style="position: relative;box-shadow: 0 0 7px 2px <?= $color; ?>;">
                             <div class="progress-bar" role="progressbar" style="width: <?= min($percentage, 100); ?>%;"
                                 aria-valuenow="<?= $percentage; ?>" aria-valuemin="0" aria-valuemax="100"></div>
                             <div class="progress-marks"
                                 style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
                                 <div class="progress-mark"
-                                    style="position: absolute; top: 0; left: <?= $silverPercentage; ?>%; width: 2px; height: 100%; background-color: #fff;">
+                                    style="position: absolute; top: 0; left: <?= $silverPercentage; ?>%; width: 2px; height: 100%; background-color: #000;">
                                 </div>
                                 <div class="progress-mark"
-                                    style="position: absolute; top: 0; left: <?= $goldPercentage; ?>%; width: 2px; height: 100%; background-color: #fff;">
+                                    style="position: absolute; top: 0; left: <?= $goldPercentage; ?>%; width: 3px; height: 100%; background-color: #000;">
                                 </div>
                                 <div class="progress-mark"
-                                    style="position: absolute; top: 0; left: <?= $platinumPercentage; ?>%; width: 1px; height: 100%; background-color: #fff;">
+                                    style="position: absolute; top: 0; left: <?= $platinumPercentage; ?>%; width: 2px; height: 100%; background-color: #000;">
                                 </div>
                             </div>
                         </div>
@@ -195,7 +204,8 @@ getUser();
                         <div class="container">
                             <small class="text-sm mt-2 text-white">Số tiền còn thiếu để đạt <br> <span
                                     class="fw-bold"><?= $level ?></span></small>
-                            <p class="fw-bold fst-italic mb-0"><?= number_format(max(0, $diff), 0, ',', '.') ?> ₫</p>
+                            <p class="fw-bold fs-6 mb-0">
+                                <?= number_format(max(0, $diff), 0, ',', '.') ?> ₫</p>
                             <!-- Đảm bảo không có giá trị âm -->
                         </div>
                     </div>
@@ -227,8 +237,8 @@ getUser();
                                         <div class="mb-4">
                                             <label for="fullName" class="form-label fw-semibold">Họ và tên</label>
                                             <?php if (isset($messages['tennd'])): ?>
-                                            <small
-                                                class="text-danger m-2 text-xs"><?= htmlspecialchars($messages['tennd']) ?></small>
+                                                <small
+                                                    class="text-danger m-2 text-xs"><?= htmlspecialchars($messages['tennd']) ?></small>
                                             <?php endif; ?>
                                             <div class="input-group">
                                                 <span class="input-group-text bg-light border-0"><i
@@ -242,8 +252,8 @@ getUser();
                                         <div class="mb-4">
                                             <label for="email" class="form-label fw-semibold">Email</label>
                                             <?php if (isset($messages['email'])): ?>
-                                            <small
-                                                class="text-danger m-2 text-xs"><?= htmlspecialchars($messages['email']) ?></small>
+                                                <small
+                                                    class="text-danger m-2 text-xs"><?= htmlspecialchars($messages['email']) ?></small>
                                             <?php endif; ?>
                                             <div class="input-group">
                                                 <span class="input-group-text bg-light border-0"><i
--- views/transition-history.php
+++ views/transition-history.php
@@ -1,5 +1,6 @@
  <table class="table table-striped table-hover">
      <thead>
+         <small class="text-danger">*Chi tiêu 10 ngày gần nhất</small>
          <tr class="text-center">
              <th>STT</th>
              <th>Ngày mua</th>
@@ -8,21 +9,21 @@
          </tr>
      </thead>
      <tbody>
-        <?php
-        $list_bill = getBillByUserId($NDId);
+         <?php
+            $list_bill = getBillByUserId($NDId);
 
-        if (!empty($list_bill)) {
-            foreach ($list_bill as $index => $bill) {
-                echo "<tr>";
-                echo "<td class='text-center'>" . ($index + 1) . "</td>";
-                echo "<td class='text-center'>" . htmlspecialchars($bill['NgayLapHD']) . "</td>"; // Hiển thị ngày mua
-                echo "<td class='text-center'>" . htmlspecialchars($bill['TenPhim']) . "</td>"; // Hiển thị tên phim
-                echo "<td class='text-center'>" . number_format($bill['TongTien'], 0, ',', '.') . " VND</td>"; // Hiển thị tổng tiền
-                echo "</tr>";
+            if (!empty($list_bill)) {
+                foreach ($list_bill as $index => $bill) {
+                    echo "<tr>";
+                    echo "<td class='text-center'>" . ($index + 1) . "</td>";
+                    echo "<td class='text-center'>" . htmlspecialchars($bill['NgayLapHD']) . "</td>"; // Hiển thị ngày mua
+                    echo "<td class='text-center'>" . htmlspecialchars($bill['TenPhim']) . "</td>"; // Hiển thị tên phim
+                    echo "<td class='text-center text-success fw-bolder'>" . number_format($bill['TongTien'], 0, ',', '.') . " VNĐ</td>"; // Hiển thị tổng tiền
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='4' class='text-center'>Chưa có giao dịch nào.</td></tr>";
             }
-        } else {
-            echo "<tr><td colspan='4' class='text-center'>Chưa có giao dịch nào.</td></tr>";
-        }
-        ?>
+            ?>
      </tbody>
  </table>
